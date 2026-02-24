5d9a4f55721b3b2cf80c42ab78617060cdac51a7
Kalle Hirvijärvi <kalle.hirvijarvi@tuni.fi>
2026-02-24 17:08:24 +0200

    added simpleble library

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
-
+#include <simpleble/SimpleBLE.h>
 #include <iostream>
 
-int main(){
-    std::cout << "Hello world!\n";
-    return 0;
+int main() {
+    auto adapters = SimpleBLE::Adapter::get_adapters();
+
+    std::cout << "Found " << adapters.size() << " adapters\n";
+
+    for (auto& adapter : adapters) {
+        std::cout << "Adapter: " << adapter.identifier() << "\n";
+    }
 }
