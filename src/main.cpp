2f0173e2c1a480f334c730d266049b6ca5891e69
Kalle Hirvijärvi <kalle.hirvijarvi@tuni.fi>
2026-03-06 14:53:57 +0200

    indicate to notify

--- src/main.cpp
+++ src/main.cpp
@@ -92,10 +92,11 @@ void connect_device(SimpleBLE::Peripheral pref){
    try {
       pref.set_callback_on_disconnected([pref_type](){disconnect_handler(pref_type);});
       if (pref_type == REMOTE){
-         pref.indicate(SERVICE_ID_REMOTE, CHAR_ID_REMOTE_PRESS_BUTTON, [pref_type] (SimpleBLE::ByteArray bytes){request_handler(bytes, pref_type);});
+         broadcast_state();
+         pref.notify(SERVICE_ID_REMOTE, CHAR_ID_REMOTE_PRESS_BUTTON, [pref_type] (SimpleBLE::ByteArray bytes){request_handler(bytes, pref_type);});
       }
       else {
-         pref.indicate(SERVICE_ID_SENSOR, CHAR_ID_SENSOR_TRIGGER, [pref_type] (SimpleBLE::ByteArray bytes){request_handler(bytes, pref_type);});
+         pref.notify(SERVICE_ID_SENSOR, CHAR_ID_SENSOR_TRIGGER, [pref_type] (SimpleBLE::ByteArray bytes){request_handler(bytes, pref_type);});
       }
    } catch (const std::exception& e){
       std::cout << "UUID matched but initialization failed:\n" << e.what() << std::endl;
