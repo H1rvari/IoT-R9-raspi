2d91ae2fdcd3129187612469821cc4f52338b1e3
Kalle Hirvijärvi <kalle.hirvijarvi@tuni.fi>
2026-03-07 13:53:06 +0200

    get and user service and char uuids

--- src/main.cpp
+++ src/main.cpp
@@ -110,11 +110,11 @@ void connect_device(SimpleBLE::Peripheral& pref){
    try {
       pref.set_callback_on_disconnected([pref_type](){disconnect_handler(pref_type);});
       if (pref_type == REMOTE){
-         pref.notify(SERVICE_ID_REMOTE, CHAR_ID_REMOTE_PRESS_BUTTON, [pref_type] (SimpleBLE::ByteArray bytes){request_handler(bytes, pref_type);});
+         pref.notify(pref.services()[0].uuid(), pref.services()[0].characteristics[0].uuid(), [pref_type] (SimpleBLE::ByteArray bytes){request_handler(bytes, pref_type);});
          broadcast_state();
       }
       else {
-         pref.notify(SERVICE_ID_SENSOR, CHAR_ID_SENSOR_TRIGGER, [pref_type] (SimpleBLE::ByteArray bytes){request_handler(bytes, pref_type);});
+         pref.notify(pref.services()[0].uuid(), pref.services()[0].characteristics[0].uuid(), [pref_type] (SimpleBLE::ByteArray bytes){request_handler(bytes, pref_type);});
       }
    } catch (const std::exception& e){
       std::cout << "UUID matched but initialization failed:\n" << e.what() << std::endl;
