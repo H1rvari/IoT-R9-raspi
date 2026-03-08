89d72dada0bf1f57b59f61fd1001cb03e8bc0a75
Kalle Hirvijärvi <kalle.hirvijarvi@tuni.fi>
2026-03-08 12:16:44 +0200

    syntax

--- src/main.cpp
+++ src/main.cpp
@@ -110,7 +110,7 @@ void connect_device(SimpleBLE::Peripheral pref_temp){
       else {
          sensor_initialized = false;
       }
-      adapter.scan_on();
+      adapter.scan_start();
       return;
    }
 
