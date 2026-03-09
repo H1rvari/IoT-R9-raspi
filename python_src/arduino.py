970c358fedb2a43949d0959d90ae6a2795529a7d
Kalle Hirvijärvi <kalle.hirvijarvi@tuni.fi>
2026-03-09 11:56:50 +0200

    lte's try this

--- python_src/arduino.py
+++ python_src/arduino.py
@@ -56,7 +56,7 @@ def on_remote_press(sender, data):
         asyncio.create_task(alarm.update_remote_status())
 
 # --- Connection Manager ---
-async def manage_device(address, name):
+async def manage_device_remote(address, name):
     """Handles connection, notification setup, and reconnection for one device."""
     while True:
         async with alarm.lock: # Ensure we don't collide during connection attempts
@@ -97,13 +97,55 @@ async def manage_device(address, name):
                 await alarm.update_remote_status()
                 await asyncio.sleep(2)
 
+# --- Connection Manager ---
+async def manage_device_sensor(address, name):
+    """Handles connection, notification setup, and reconnection for one device."""
+    while True:
+        async with alarm.lock: # Ensure we don't collide during connection attempts
+            print(f"Scanning for {name} ({address})...")
+            device = await BleakScanner.find_device_by_address(address, timeout=5.0)
+            
+            if not device:
+                await asyncio.sleep(2)
+                continue
+
+            try:
+                async with BleakClient(device) as client:
+                    print(f"Connected to {name}")
+                    
+                    if address == SENSOR_ADDR:
+                        alarm.is_active = True
+                        await client.start_notify(CHAR_ID_SENSOR_TRIGGER, on_sensor_data)
+                    else:
+                        alarm.remote_client = client
+                        await client.start_notify(CHAR_ID_REMOTE_PRESS, on_remote_press)
+                    
+                    await alarm.update_remote_status()
+
+                    while client.is_connected:
+                        await asyncio.sleep(1)
+
+                    # Keep alive until disconnect
+                    await manage_device_remote(REMOTE_ADDR, "REMOTE"),
+            except Exception as e:
+                print(f"Error in {name} loop: {e}")
+            finally:
+                if address == SENSOR_ADDR:
+                    alarm.is_active = False
+                    alarm.is_armed = False
+                    alarm.alarm_on = False
+                else:
+                    alarm.remote_client = None
+                
+                print(f"{name} disconnected. Re-scanning...")
+                await alarm.update_remote_status()
+                await asyncio.sleep(2)
+
 
 async def main():
-    # Run Remote and Sensor managers concurrently
-    await asyncio.gather(
-        manage_device(REMOTE_ADDR, "REMOTE"),
-        manage_device(SENSOR_ADDR, "SENSOR")
-    )
+
+    manage_device_sensor(SENSOR_ADDR, "SENSOR")
+    
 
 
 if __name__ == "__main__":
